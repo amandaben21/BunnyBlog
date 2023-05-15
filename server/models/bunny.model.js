480226b910e7de11aef5dae6194f07045eaeb763
Amanda Contreras <amandaben21@aol.com>
2023-05-14 20:41:50 -0400

    Finished creating my backend for bunny blog. Now about to start the frontend.

---
+++ server/models/bunny.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const BunnySchema = new mongoose.Schema({
+    title:{
+        type: String,
+        require: [true, "Title of Bunny Blog is required"],
+        minlength: [3, "Title must be at least 3 characters long"]
+    },
+    //name of person who upload
+    name:{
+        type: String,
+        require: [true, "Your name is required"],
+        minlength: [3, "Your name must be at least 3 characters long"]
+    },
+
+    desc:{
+        type: String,
+        require: [true, "Your writing is required"],
+        minlength: [50, "Your blog must be at least 50 characters long"]
+    },
+
+}, { timestamps: true })
+
+module.exports = mongoose.model("bunny", BunnySchema);
